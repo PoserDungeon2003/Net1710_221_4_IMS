1c53ea2c40051610738f84743d9bea79416a8d25
Danh Tran <gioantrandanh@gmail.com>
2024-05-22 14:31:40 +0700

    CRUD interviewsInfo

---
+++ IMS.RazorWebApp/Pages/Interviews/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using IMS.Data.Models;
+using IMS.Data.Repository;
+
+namespace IMS.RazorWebApp.Pages.Interviews
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly IMS.Data.Repository.Net1710_221_4_IMSContext _context;
+
+        public DeleteModel(IMS.Data.Repository.Net1710_221_4_IMSContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public InterviewsInfo InterviewsInfo { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var interviewsinfo = await _context.InterviewsInfos.FirstOrDefaultAsync(m => m.InterviewinfoId == id);
+
+            if (interviewsinfo == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                InterviewsInfo = interviewsinfo;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var interviewsinfo = await _context.InterviewsInfos.FindAsync(id);
+            if (interviewsinfo != null)
+            {
+                InterviewsInfo = interviewsinfo;
+                _context.InterviewsInfos.Remove(InterviewsInfo);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
