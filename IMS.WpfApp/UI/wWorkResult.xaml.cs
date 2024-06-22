2e6d81fa09c7acdefc666cc1cdf847f21f2806f9
@giangnntse173469 <giangnntse173469@fpt.edu.vn>
2024-06-22 15:59:01 +0700

    Work RS wpf main fnc

--- IMS.WpfApp/UI/wWorkResult.xaml.cs
+++ IMS.WpfApp/UI/wWorkResult.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel;
+using System.Xml.Serialization;
 
 namespace IMS.WpfApp.UI
 {
@@ -25,6 +26,9 @@ namespace IMS.WpfApp.UI
     public partial class wWorkResult : Window
     {
         private readonly WorkingResultBusiness _workingResultBusiness;
+        public WorkingResult workingResult { get; set; }
+
+        private bool _suppressSelectionChanged = false;
 
 
         public wWorkResult()
@@ -32,7 +36,7 @@ namespace IMS.WpfApp.UI
             _workingResultBusiness ??= new WorkingResultBusiness();
             InitializeComponent();
             LoadGrdWorkResult();
-            
+            workingResult = new WorkingResult();
         }
 
        public async void LoadGrdWorkResult()
@@ -47,38 +51,140 @@ namespace IMS.WpfApp.UI
                 MessageBox.Show(result.Message, "Load");
             }
         }
+        public void LoadGrdWorkResultBox(WorkingResult workingResult)
+        {
+            txtCreatedBy.Text = workingResult.CreatedBy;
+            txtRating.Text = workingResult.Rating.ToString();
+            txtNote.Text = workingResult.Note;
+            txtDateCompleted.Text = workingResult.DateCompleted.ToString();
+            txtStatus.Text = workingResult.Status;
+            txtHoursWork.Text = workingResult.HoursWorked.ToString();
+            txtMentorID.Text = workingResult.MentorId.ToString();
+            txtTaskID.Text = workingResult.TaskId.ToString();
+            txtInternID.Text = workingResult.InternId.ToString();
+        }
 
 
         private void ButtonCancel_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                txtCreatedBy.Text = "";
+                txtRating.Text = "";
+                txtNote.Text = "";
+                txtDateCompleted.Text = "";
+                txtStatus.Text = "";
+                txtHoursWork.Text = "";
+                txtMentorID.Text = "";
+                txtTaskID.Text = "";
+                txtInternID.Text = "";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Cancel");
+            }
         }
 
         private void ButtonSave_Click(object sender, RoutedEventArgs e)
         {
-            var WorkResult = new WorkingResult()
+            try
             {
-                CreatedBy = txtCreatedBy.Text,
-                Rating = double.Parse(txtRating.Text),
-                Note = txtNote.Text,
-                DateCompleted = DateTime.Parse(txtDateCompleted.Text),
-                Status = txtStatus.Text,
-                HoursWorked = int.Parse(txtHoursWork.Text),
-                MentorId = int.Parse(txtMentorID.Text),
-                TaskId = int.Parse(txtTaskID.Text),
-                InternId = int.Parse(txtInternID.Text),
-            };
-            _workingResultBusiness.AddAsync(WorkResult);
-            LoadGrdWorkResult();
-        }
-
-        private void txtResultID_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
+                var WorkResult = new WorkingResult()
+                {
+                    CreatedBy = txtCreatedBy.Text,
+                    Rating = double.Parse(txtRating.Text),
+                    Note = txtNote.Text,
+                    DateCompleted = DateTime.Parse(txtDateCompleted.Text),
+                    Status = txtStatus.Text,
+                    HoursWorked = int.Parse(txtHoursWork.Text),
+                    MentorId = int.Parse(txtMentorID.Text),
+                    TaskId = int.Parse(txtTaskID.Text),
+                    InternId = int.Parse(txtInternID.Text),
+                };
+                _workingResultBusiness.AddAsync(WorkResult);
+                LoadGrdWorkResult();
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Save");
+            }
+            
         }
 
         private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                if (grdWorkResult.SelectedItem != null)
+                {
+                    var selectedResult = (WorkingResult)grdWorkResult.SelectedItem;
+                    selectedResult.CreatedBy = txtCreatedBy.Text;
+                    selectedResult.Rating = double.Parse(txtRating.Text);
+                    selectedResult.Note = txtNote.Text;
+                    selectedResult.DateCompleted = DateTime.Parse(txtDateCompleted.Text);
+                    selectedResult.Status = txtStatus.Text;
+                    selectedResult.HoursWorked = int.Parse(txtHoursWork.Text);
+                    selectedResult.MentorId = int.Parse(txtMentorID.Text);
+                    selectedResult.TaskId = int.Parse(txtTaskID.Text);
+                    selectedResult.InternId = int.Parse(txtInternID.Text);
+
+                    _workingResultBusiness.Update(selectedResult);
+                    LoadGrdWorkResult();
+                }
+                else
+                {
+                    MessageBox.Show("Please select a working result to update.", "Update");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Update");
+            }
+        }
+
+        private void grdWorkResult_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_suppressSelectionChanged)
+            {
+                return;
+            }
+            try
+            {
+                workingResult.ResultId = (grdWorkResult.SelectedItem as WorkingResult).ResultId;
+                workingResult.CreatedBy = (grdWorkResult.SelectedItem as WorkingResult).CreatedBy;
+                workingResult.Rating = (grdWorkResult.SelectedItem as WorkingResult).Rating;
+                workingResult.Note = (grdWorkResult.SelectedItem as WorkingResult).Note;
+                workingResult.DateCompleted = (grdWorkResult.SelectedItem as WorkingResult).DateCompleted;
+                workingResult.Status = (grdWorkResult.SelectedItem as WorkingResult).Status;
+                workingResult.HoursWorked = (grdWorkResult.SelectedItem as WorkingResult).HoursWorked;
+                workingResult.MentorId = (grdWorkResult.SelectedItem as WorkingResult).MentorId;
+                workingResult.TaskId = (grdWorkResult.SelectedItem as WorkingResult).TaskId;
+                workingResult.InternId = (grdWorkResult.SelectedItem as WorkingResult).InternId;
+                LoadGrdWorkResultBox(workingResult);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _suppressSelectionChanged = true;
+                var result = _workingResultBusiness.Delete(workingResult);
+                LoadGrdWorkResult();
+                grdWorkResult.UnselectAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            finally
+            {
+                _suppressSelectionChanged = false;
+            }
 
         }
     }
